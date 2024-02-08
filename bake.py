0972afa3c0a1ebbfd1bf9141731c3701b55f0bc6
Hugh Perkins <hughperkins@gmail.com>
2024-02-08 05:58:09 -0500

    Update bake.py

--- bake.py
+++ bake.py
@@ -3,5 +3,20 @@
 
 import bpy
 
-bpy.ops.fluid.bake_all()
+import bpy
+
+for scene in bpy.data.scenes:
+    print('scene', scene)
+    for object in scene.objects:
+        print('object', object)
+        for modifier in object.modifiers:
+            print('modifier', modifier, 'modifier_type', modifier.type)
+            if modifier.type == 'FLUID':
+                print('modifier.fluid_type', modifier.fluid_type)
+                if modifier.fluid_type == 'DOMAIN':
+                    print('baking...')
+                    object.select_set(True)
+                    bpy.context.view_layer.objects.active = object
+                    bpy.ops.fluid.bake_all()
+
 bpy.ops.wm.save_mainfile()
